7efb8b9e92f0d67f43d6f673bcc10f314a244a2d
kslisenko <kslisenko@gmail.com>
2018-03-24 20:45:50 +0300

    Finalized nginx + tomcat deployment (without static content)

--- nginx-tomcat/demo/src/main/java/com/example/demo/DemoApplication.java
+++ nginx-tomcat/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -2,6 +2,8 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,9 +12,14 @@ import java.util.List;
 
 @RestController
 @SpringBootApplication
-public class DemoApplication {
+public class DemoApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DemoApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
